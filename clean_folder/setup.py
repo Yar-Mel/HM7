04c5c8dd4a5bbe2603a94ef02658ddd02e3977ab
Yaroslav Melnychuk <yarmel.dev@gmail.com>
2023-04-16 13:28:56 +0300

    first commit

---
+++ clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='0.0.1',
+    description='file sorter for specific extensions',
+    url='https://github.com/Yar-Mel/HM7',
+    author='Yaroslav Melnychuk',
+    author_email='yarmel.dev@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:clean']}
+)
